d99c010e4e8b57b9a48485870cdd8a8804edbd9c
Dinesh R <52753312+dinesh1101@users.noreply.github.com>
2021-05-29 21:03:12 +0530

    Id solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+//import { Button } from "react-bootstrap";
 
 //import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
